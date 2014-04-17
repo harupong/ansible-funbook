8068012fa13e05c6ecc3b402029cc5ed6bf04921
harupong <harupong@gmail.com>
2014-04-17 18:29:56 +0900

    Write my.lock when starting recording

---
+++ start_recording.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -f my.lock ]; then
+  date >> /root/my.lock
+else
+  touch my.lock
+  date >> /root/my.lock
+fi
+
+/bin/bash -l -c "cd /root/ripdiko && ruby bin/ripdiko $1 >> /root/ripdiko_$1.log 2>&1"
